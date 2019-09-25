ff7d3181bd3f5dc6fc0e930c18573db7271341b5
Steve Tarzia <steve@stevetarzia.com>
2019-09-25 13:16:33 -0500

    replaced tab with spaces

--- src/main/java/edu/northwestern/ssa/App.java
+++ src/main/java/edu/northwestern/ssa/App.java
@@ -2,6 +2,6 @@ package edu.northwestern.ssa;
 
 public class App {
     public static void main(String[] args) {
-	System.out.println("Hello world!");
+        System.out.println("Hello world!");
     }
 }
